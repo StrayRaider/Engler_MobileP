a9ce9059361257c1d080e7dbcb647de646cae4df
Elif Aslan <elfaslnvon@gmail.com>
2024-12-23 01:12:27 +0300

    login, signin, camera, wordlist activities added

--- app/src/main/java/com/example/engler/MainActivity.kt
+++ app/src/main/java/com/example/engler/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.example.engler
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.widget.EditText
+import androidx.recyclerview.widget.RecyclerView.EdgeEffectFactory.EdgeDirection
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val userName = findViewById<EditText>(R.id.etMail)
+        val password = findViewById<EditText>(R.id.etPassword)
+        val submitButton = findViewById<Button>(R.id.btnSubmit)
+        val signInBtn = findViewById<Button>(R.id.btnSignIn)
+        val cameraBtn = findViewById<Button>(R.id.btnCamera)
+
+        val loginUser = Login(this, submitButton, userName, password, signInBtn, cameraBtn)
+
+        loginUser.setupButtonClickListener()
+        loginUser.signInButtonClickListener()
+        loginUser.cameraButtonClickListener()
     }
 }
